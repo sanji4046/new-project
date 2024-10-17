ad6bc932beab77ed49624e737eb67430631378ce
sanji4046 <amk1004108@student.hamk.fi>
2024-10-17 18:01:12 +0300

    Initial commit

---
+++ src/AgeApp.java
@@ -0,0 +1,16 @@
+
+        public class AgeApp {
+            public static void main(String[] args) {
+                int age = 1; 
+        
+                if (age > 0 && age < 18) {
+                    System.out.println("You are underage.");
+                } else {
+                    System.out.println("You are an adult.");
+                }
+        
+                System.out.println("Press space to exit.");
+            }
+        }
+        
+    
