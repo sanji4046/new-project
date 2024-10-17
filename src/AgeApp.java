3ee30f5755fec9c5217d4e2f4d3d31330df39844
sanji4046 <amk1004108@student.hamk.fi>
2024-10-17 18:29:41 +0300

    Adding Mopeds

--- src/AgeApp.java
+++ src/AgeApp.java
@@ -1,9 +1,12 @@
 public class AgeApp {
     public static void main(String[] args) {
-        int age = 75; 
+        int age = 17; 
 
         if (age > 0 && age < 18) {
             System.out.println("You are underage.");
+            if (age >= 15) {
+                System.out.println("You can drive a moped.");
+            }
         } else if (age >= 65) {
             System.out.println("You are retired.");
         } else {
