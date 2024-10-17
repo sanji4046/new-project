519c3dff4cba2e2c6bb3968c1c865c2dbc28dda4
sanji4046 <amk1004108@student.hamk.fi>
2024-10-17 18:18:52 +0300

    add retirement  code

--- src/AgeApp.java
+++ src/AgeApp.java
@@ -1,16 +1,15 @@
+public class AgeApp {
+    public static void main(String[] args) {
+        int age = 75; 
 
-        public class AgeApp {
-            public static void main(String[] args) {
-                int age = 1; 
-        
-                if (age > 0 && age < 18) {
-                    System.out.println("You are underage.");
-                } else {
-                    System.out.println("You are an adult.");
-                }
-        
-                System.out.println("Press space to exit.");
-            }
+        if (age > 0 && age < 18) {
+            System.out.println("You are underage.");
+        } else if (age >= 65) {
+            System.out.println("You are retired.");
+        } else {
+            System.out.println("You are an adult.");
         }
-        
-    
+
+        System.out.println("Press space to exit.");
+    }
+}
